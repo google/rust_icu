5acc7aed32829c28c3ea6a18526fcb381adc6acb
Filip Filmar <fmil@google.com>
2021-05-03 20:08:24 +0000

    Implements ECMA402 compatible date and time formatting.
    Adds ECMA 402 compatible options for date and time formatting.  This is
    not 100% aligned with ECMA since some options are not quite clear.  But
    it is close enough, and can be used out of the box.
    
    Fixes: #135

--- rust_icu_ecma402/src/datetimeformat.rs
+++ rust_icu_ecma402/src/datetimeformat.rs
@@ -29,13 +29,180 @@ pub struct DateTimeFormat {
 
 pub(crate) mod internal {
     use ecma402_traits::datetimeformat::DateTimeFormatOptions;
+    use ecma402_traits::datetimeformat::options;
     use rust_icu_common as common;
     use rust_icu_ustring as ustring;
+    use rust_icu_uloc as uloc;
     use std::convert::TryFrom;
+    use anyhow::anyhow;
 
-    // TODO: implement this conversion completely.
-    pub fn opt_to_pattern(_opts: DateTimeFormatOptions) -> Result<ustring::UChar, common::Error> {
-        ustring::UChar::try_from("YYYY")
+    fn check_auto_styling_set(opts: &DateTimeFormatOptions) -> Result<(), common::Error> {
+        if opts.date_style.is_some() || opts.time_style.is_some() {
+            return Err(common::Error::Wrapper(
+                anyhow!("can't use custom styling because date_style or time_style is set")));
+        }
+        Ok(())
+    }
+
+    /// The skeleton pattern components come from:
+    /// <https://unicode-org.github.io/icu/userguide/format_parse/datetime/#datetimepatterngenerator>
+    pub fn skeleton_from_opts(opts: &DateTimeFormatOptions) -> Result<ustring::UChar, common::Error> {
+        let mut skel = vec![];
+        // Looks like date and time style are mutually exclusive with other
+        // settings.
+        if let Some(ref date_style) = &opts.date_style {
+            match date_style {
+                options::Style::Full => {}
+                options::Style::Long => {}
+                options::Style::Medium => {}
+                options::Style::Short => {}
+            }
+        }
+        if let Some(ref time_style) = &opts.time_style {
+            match time_style {
+                options::Style::Full => {}
+                options::Style::Long => {}
+                options::Style::Medium => {}
+                options::Style::Short => {}
+            }
+        }
+        if let Some(ref digits) = &opts.fractional_second_digits {
+            check_auto_styling_set(opts)?;
+            match digits.get() {
+                1 => skel.push("S"),
+                2 => skel.push("SS"),
+                3 => skel.push("SSS"),
+                _ => skel.push("SSSS"),
+            }
+        }
+        if let Some(ref day_period) = &opts.day_period {
+            check_auto_styling_set(opts)?;
+            match day_period {
+                options::DayPeriod::Short => skel.push("a"),
+                // Not obvious for other match arms.
+                options::DayPeriod::Long => {},
+                options::DayPeriod::Narrow => {}
+            }
+        }
+        if let Some(ref weekday) = &opts.weekday {
+            check_auto_styling_set(opts)?;
+            match weekday {
+                options::Weekday::Long => skel.push("EEEE"),
+                options::Weekday::Short => skel.push("EEE"),
+                options::Weekday::Narrow => skel.push("EEEEEE"),
+            }
+        }
+        if let Some(ref era) = &opts.era {
+            check_auto_styling_set(opts)?;
+            match era {
+                options::Era::Long => skel.push("GGGG"),
+                options::Era::Short => skel.push("GG"),
+                options::Era::Narrow => skel.push("GGGGG"),
+            }
+        }
+        if let Some(ref year) =&opts.year {
+            check_auto_styling_set(opts)?;
+            match year {
+                options::DisplaySize::Numeric => skel.push("yyyy"),
+                options::DisplaySize::TwoDigit => skel.push("yy"),
+            }
+        }
+        if let Some(ref month) = &opts.month {
+            check_auto_styling_set(opts)?;
+            match month {
+                options::Month::Numeric => skel.push("M"),
+                options::Month::TwoDigit => skel.push("MM"),
+                options::Month::Short => skel.push("MMM"),
+                options::Month::Long => skel.push("MMMM"),
+                options::Month::Narrow => skel.push("MMMMM"),
+            }
+        }
+        if let Some(day) = &opts.day {
+            check_auto_styling_set(opts)?;
+            match day {
+                options::DisplaySize::Numeric => skel.push("d"),
+                options::DisplaySize::TwoDigit => skel.push("dd"),
+            }
+        }
+        if let Some(hour) = &opts.hour {
+            check_auto_styling_set(opts)?;
+            if let Some(hour_cycle) = &opts.hour_cycle {
+                match hour_cycle {
+                    options::HourCycle::H11 => 
+                        match hour {
+                            options::DisplaySize::Numeric => skel.push("K"),
+                            options::DisplaySize::TwoDigit => skel.push("KK"),
+                    },
+                    options::HourCycle::H12 => 
+                        match hour {
+                            options::DisplaySize::Numeric => skel.push("h"),
+                            options::DisplaySize::TwoDigit => skel.push("hh"),
+                        },
+                    options::HourCycle::H23 =>
+                        match hour {
+                            options::DisplaySize::Numeric => skel.push("H"),
+                            options::DisplaySize::TwoDigit => skel.push("HH"),
+                        },
+                    options::HourCycle::H24 => match hour {
+                            options::DisplaySize::Numeric => skel.push("k"),
+                            options::DisplaySize::TwoDigit => skel.push("kk"),
+                        }
+                }
+            } else {
+                // This may not be correct for 23/24 hours locale default.
+                match hour {
+                    options::DisplaySize::Numeric => skel.push("h"),
+                    options::DisplaySize::TwoDigit => skel.push("hh"),
+                }
+            }
+        }
+        if let Some(minute) = &opts.minute {
+            check_auto_styling_set(opts)?;
+            match minute {
+                options::DisplaySize::Numeric => skel.push("m"),
+                options::DisplaySize::TwoDigit => skel.push("mm"),
+            }
+        }
+        if let Some(second) = &opts.second {
+            check_auto_styling_set(opts)?;
+            match second {
+                options::DisplaySize::Numeric => skel.push("s"),
+                options::DisplaySize::TwoDigit => skel.push("ss"),
+            }
+        }
+        if let Some(time_zone_style) = &opts.time_zone_style {
+            check_auto_styling_set(opts)?;
+            match time_zone_style {
+                options::TimeZoneStyle::Long => skel.push("zzzz"),
+                options::TimeZoneStyle::Short => skel.push("O"),
+            }
+        }
+        let concat: String = skel.join(" ");
+        ustring::UChar::try_from(&concat[..])
+    }
+    
+    // Modifies the input locale based on the input pattern.  The settings here can only
+    // be set on the locale used for formatting, not on the date time formatting pattern.
+    //
+    // Note: this only works with the BCP47 timezones ("uslax", not "America/Los_Angeles").
+    pub fn locale_from_opts(locale: uloc::ULoc, opts: &DateTimeFormatOptions) -> uloc::ULoc {
+        let mut locale = uloc::ULocMut::from(locale);
+        
+        if let Some(calendar) = &opts.calendar {
+            locale.set_unicode_keyvalue("ca", &calendar.0);
+        }
+        if let Some(n) = &opts.numbering_system {
+            locale.set_unicode_keyvalue("nu", &n.0);
+        }
+        if let Some(tz) = &opts.time_zone {
+            locale.set_unicode_keyvalue("tz", &tz.0);
+        }
+        if let Some(h) = &opts.hour_cycle {
+            locale.set_unicode_keyvalue("hc", &format!("{}", h));
+        }
+        // This will panic if any of the settings above are invalid.
+        let loc = uloc::ULoc::from(locale);
+        loc
     }
 }
 
@@ -54,12 +221,17 @@ impl ecma402_traits::datetimeformat::DateTimeFormat for DateTimeFormat {
         L: ecma402_traits::Locale,
         Self: Sized,
     {
-        let locale: &str = &format!("{}", l);
-        let locale = uloc::ULoc::try_from(locale)?;
-        let pattern = internal::opt_to_pattern(opts)?;
-        // tz_id needs to be  empty per ECMA402 spec.  If you need an alternate
-        // timezone, use a unicode "-tz-" extension on the locale.
-        let tz_id = ustring::UChar::try_from("")?;
+        let locale = internal::locale_from_opts(
+            uloc::ULoc::try_from(&format!("{}", l)[..])?, &opts);
+        let gen = udat::UDatePatternGenerator::new(&locale)?;
+
+        let pattern = gen.get_best_pattern_ustring(
+            &internal::skeleton_from_opts(&opts)?)?;
+
+        // The correct timezone ID comes from the resulting locale.
+        let tz_id = locale.keyword_value("timezone")?.or(Some("".to_owned())).unwrap();
+        let tz_id = ustring::UChar::try_from(&tz_id[..])?;
+
         let rep = udat::UDateFormat::new_with_pattern(&locale, &tz_id, &pattern)?;
         Ok(DateTimeFormat { rep })
     }
@@ -84,7 +256,7 @@ impl ecma402_traits::datetimeformat::DateTimeFormat for DateTimeFormat {
 #[cfg(test)]
 mod testing {
     use super::*;
-    use ecma402_traits::{datetimeformat::DateTimeFormat, datetimeformat::DateTimeFormatOptions};
+    use ecma402_traits::datetimeformat::{options, DateTimeFormat, DateTimeFormatOptions, };
     use rust_icu_sys as usys;
     use rust_icu_uloc as uloc;
     use std::convert::TryFrom;
@@ -98,13 +270,106 @@ mod testing {
             dates: Vec<usys::UDate>,
             expected: Vec<&'static str>,
         }
-        let tests = vec![TestCase {
-            locale: "sr_RS-u-tz-uslax",
-            opts: Default::default(),
-            dates: vec![10000_f64],
-            // TBD
-            expected: vec!["1970"],
-        }];
+        let tests = vec![
+            TestCase {
+                locale: "sr-RS",
+                opts: DateTimeFormatOptions{
+                    year:  Some(options::DisplaySize::Numeric),
+                    ..Default::default()
+                },
+
+                dates: vec![10000_f64],
+                expected: vec!["1970."],
+            },
+            // In ICU 63 this gets reported as "GMT-08:00", likely the ICU data
+            // from then didn't contain the Serbian long spellout of the "uslax"
+            // time zone.  Turning on from 67 onwards, since at the time this
+            // test was written that was the oldest version that did have the
+            // long spellout.
+            #[cfg(feature="icu_version_67_plus")]
+            TestCase {
+                locale: "sr",
+                opts: DateTimeFormatOptions{
+                    time_zone: Some(options::TimeZone("uslax".to_owned())),
+                    time_zone_style: Some(options::TimeZoneStyle::Long),
+                    ..Default::default()
+                },
+
+                dates: vec![10000_f64],
+                expected: vec!["Северноамеричко пацифичко стандардно време"],
+            },
+            TestCase {
+                locale: "en-US",
+                opts: DateTimeFormatOptions{
+                    time_zone: Some(options::TimeZone("uslax".to_owned())),
+                    weekday: Some(options::Weekday::Long),
+                    year:  Some(options::DisplaySize::Numeric),
+                    day: Some(options::DisplaySize::Numeric,),
+                    month: Some(options::Month::Short),
+                    hour: Some(options::DisplaySize::Numeric),
+                    minute: Some(options::DisplaySize::Numeric),
+                    second: Some(options::DisplaySize::Numeric),
+                    ..Default::default()
+                },
+
+                dates: vec![10000_f64],
+                expected: vec!["Wednesday, Dec 31, 1969, 4:00:10 PM"],
+            },
+            TestCase {
+                locale: "en-US",
+                opts: DateTimeFormatOptions{
+                    time_zone: Some(options::TimeZone("uslax".to_owned())),
+                    weekday: Some(options::Weekday::Long),
+                    year:  Some(options::DisplaySize::Numeric),
+                    day: Some(options::DisplaySize::Numeric,),
+                    month: Some(options::Month::Short),
+                    hour: Some(options::DisplaySize::Numeric),
+                    minute: Some(options::DisplaySize::Numeric),
+                    second: Some(options::DisplaySize::Numeric),
+                    time_zone_style: Some(options::TimeZoneStyle::Short),
+                    ..Default::default()
+                },
+
+                dates: vec![10000_f64],
+                expected: vec!["Wednesday, Dec 31, 1969, 4:00:10 PM GMT-8"],
+            },
+            TestCase {
+                locale: "en-US",
+                opts: DateTimeFormatOptions{
+                    time_zone: Some(options::TimeZone("nlams".to_owned())),
+                    weekday: Some(options::Weekday::Long),
+                    year:  Some(options::DisplaySize::Numeric),
+                    day: Some(options::DisplaySize::Numeric,),
+                    month: Some(options::Month::Short),
+                    hour: Some(options::DisplaySize::Numeric),
+                    minute: Some(options::DisplaySize::Numeric),
+                    second: Some(options::DisplaySize::Numeric),
+                    time_zone_style: Some(options::TimeZoneStyle::Short),
+                    ..Default::default()
+                },
+
+                dates: vec![10000_f64],
+                expected: vec!["Thursday, Jan 1, 1970, 1:00:10 AM GMT+1"],
+            },
+            TestCase {
+                locale: "en-US",
+                opts: DateTimeFormatOptions{
+                    time_zone: Some(options::TimeZone("rumow".to_owned())),
+                    weekday: Some(options::Weekday::Long),
+                    year:  Some(options::DisplaySize::Numeric),
+                    day: Some(options::DisplaySize::Numeric,),
+                    month: Some(options::Month::Short),
+                    hour: Some(options::DisplaySize::Numeric),
+                    minute: Some(options::DisplaySize::Numeric),
+                    second: Some(options::DisplaySize::Numeric),
+                    time_zone_style: Some(options::TimeZoneStyle::Short),
+                    ..Default::default()
+                },
+
+                dates: vec![10000_f64],
+                expected: vec!["Thursday, Jan 1, 1970, 3:00:10 AM GMT+3"],
+            },
+        ];
         for test in tests {
             let locale =
                 crate::Locale::FromULoc(uloc::ULoc::try_from(test.locale).expect("locale exists"));
