eac640d38bcbecb65e57cd55badac106499a9370
Filip Filmar <fmil@google.com>
2021-11-11 06:57:40 +0000

    Makes the necessary changes to have rust_icu compile with ICU 70.1
    The two changes needed were timezone data version regex upgrade and a test touch-up.
    
    This has been tested locally. Followup work will provide the builders.
    
    Issue: #223

--- rust_icu_sys/build.rs
+++ rust_icu_sys/build.rs
@@ -416,16 +416,22 @@ macro_rules! versioned_function {{
         if env::var_os("CARGO_FEATURE_ICU_VERSION_IN_ENV").is_some() {
             println!("cargo:rustc-cfg=feature=\"icu_version_in_env\"");
         }
-        let version_major = ICUConfig::version_major_int()?;
-        println!("icu-version-major: {}", version_major);
-        if version_major >= 64 {
+        let icu_major_version = ICUConfig::version_major_int()?;
+        println!("icu-version-major: {}", icu_major_version);
+        if icu_major_version >= 64 {
             println!("cargo:rustc-cfg=feature=\"icu_version_64_plus\"");
         }
-        if version_major >= 67 {
+        if icu_major_version >= 67 {
             println!("cargo:rustc-cfg=feature=\"icu_version_67_plus\"");
         }
-        if version_major >= 68 {
+        if icu_major_version >= 68 {
             println!("cargo:rustc-cfg=feature=\"icu_version_68_plus\"");
+        }
+        // Starting from version 69, the feature flags depending on the version
+        // number work for up to a certain version, so that they can be retired
+        // over time.
+        if icu_major_version <= 69 {
+            println!("cargo:rustc-cfg=feature=\"icu_version_69_max\"");
         }
         Ok(())
     }
