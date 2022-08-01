97b41496b98921dc274d72d563ee3585f5676add
SabrinaJewson <sejewson@gmail.com>
2022-08-01 11:33:08 +0100

    Reduce unnecessary changes in `bindgen/macros.rs`

--- rust_icu_sys/bindgen/macros.rs
+++ rust_icu_sys/bindgen/macros.rs
@@ -3,15 +3,15 @@
 
 /// This library was build with version renaming, so rewrite every function name
 /// with its name with version number appended.
-///
-/// The macro below will rename a symbol `foo::bar` to `foo::bar_64` (where "64")
+
+/// The macro will rename a symbol `foo::bar` to `foo::bar_64` (where "64")
 /// may be some other number depending on the ICU library in use.
-#[cfg(all(feature="renaming", not(feature="icu_version_in_env")))]
+#[cfg(all(feature="renaming",not(feature="icu_version_in_env")))]
 #[macro_export]
 macro_rules! versioned_function {
     ($i:ident) => {
       $crate::__private_do_not_use::paste::expr! {
-        $crate::[< $i _64 >]
+        $crate::[< $i _ 64 >]
       }
     }
 }
@@ -19,7 +19,7 @@ macro_rules! versioned_function {
 /// This library was build with version renaming, so rewrite every function name
 /// with its name with version number appended.
 ///
-/// The macro below will rename a symbol `foo::bar` to `foo::bar_XX` (where "XX")
+/// The macro will rename a symbol `foo::bar` to `foo::bar_XX` (where "XX")
 /// is a string coming from the environment variable RUST_ICU_MAJOR_VERSION_NUMBER,
 /// which is expected to be defined at compile time.
 #[cfg(all(feature="renaming",feature="icu_version_in_env"))]
@@ -32,7 +32,7 @@ macro_rules! versioned_function {
     }
 }
 
-// This macro will be used when no function renaming is needed
+/// This macro will be used when no function renaming is needed.
 #[cfg(not(feature="renaming"))]
 #[macro_export]
 macro_rules! versioned_function {
