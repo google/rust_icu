aba0cf6fc7558e641f1881b2be046ed924e5e8ab
Filip Filmar <fmil@google.com>
2023-10-04 14:07:04 -0700

    feat: corrects the public API for udata_open
    I realized that the stringly typed API for a `udata_open` wrapper
    leads to confusing API usages on Rust side.  I added some type
    improvements to try to fix that.
    
    This required releasing 4.2.2. I think it will be OK since nobody
    is using this code yet, and updates are trivial.

--- rust_icu_udata/src/lib.rs
+++ rust_icu_udata/src/lib.rs
@@ -40,6 +40,48 @@ enum Rep {
 unsafe impl Send for Rep {}
 unsafe impl Sync for Rep {}
 
+/// Sets the directory from which to load ICU data resources.
+///
+/// Implements `u_setDataDirectory`.
+pub fn set_data_directory(dir: &Path) {
+    let dir_cstr = ffi::CString::new
+        (dir.to_str().expect("this should never be a runtime error"))
+        .expect("this should never be a runtim error");
+    unsafe {
+        versioned_function!(u_setDataDirectory)(dir_cstr.as_ptr())
+    };
+}
+
+/// The type of the ICU resource requested.  Some standard resources have their
+/// canned types. In case you run into one that is not captured here, use [Custom],
+/// and consider sending a pull request to add the new resource type.
+pub enum Type {
+    /// An empty resource type. This is ostensibly allowed, but unclear when
+    /// it is applicable.
+    Empty,
+    /// The unpacked resource type, equivalent to "res" in ICU4C.
+    Res,
+    /// The cnv resource type, equivalent to "cnv" in ICU4C.
+    Cnv,
+    /// The "common" data type, equivalent to "dat" in ICU4C.
+    Dat,
+    /// A custom data type, in case none of the above fit your use case. It
+    /// is not clear whether this would ever be useful, but the ICU4C API
+    /// allows for it, so we must too.
+    Custom(String),
+}
+
+impl AsRef<str> for Type {
+    fn as_ref(&self) -> &str {
+        match self {
+            Type::Empty => &"",
+            Type::Res => &"res",
+            Type::Dat => &"dat",
+            Type::Cnv => &"cnv",
+            Type::Custom(ref s) => &s,
+        }
+    }
+}
 
 /// Implements `UDataMemory`.
 ///
@@ -103,16 +145,35 @@ impl crate::UDataMemory {
     /// as ostensibly the resources would be memory mapped to only the needed
     /// parts.
     ///
-    /// The `path` is the file path at which to find the resource file.
-    /// The `a_type` is the type of the resource file (can be empty).
-    /// The `name` is the name of the resource file (can also be empty).
+    /// - The `path` is the file path at which to find the resource file. Ostensibly
+    /// specifying `None` here will load from the "default" ICU_DATA path.
+    /// I have not been able to confirm this.
+    ///
+    /// - The `a_type` is the type of the resource file. It is not clear whether
+    /// the resource file type is a closed or open set, so we provide for both
+    /// possibilities.
+    ///
+    /// - The `name` is the name of the resource file. It is documented nullable
+    ///   in the ICU documentation. Pass `None` here to pass nullptr to the
+    ///   underlying C API.
+    ///
+    /// Presumably using `UDataMemory::open(Some("/dir/too"), Type::Res, Some("filename")` would
+    /// attempt to load ICU data from `/dir/too/filename.res`, as well as some other
+    /// canonical permutations of the above.  The full documentation is
+    /// [here][1], although I could not confirm that the documentation is actually
+    /// describing what the code does.  Also, using `None` at appropriate places
+    /// seems to be intended to load data from [some "default" sites][2]. I have
+    /// however observed that the actual behavior diverges from that documentation.
     ///
     /// Implements `udata_open`.
-    pub fn open(path: &Path, a_type: &str, name: &str) -> Result<Self, common::Error> {
+    ///
+    /// [1]: https://unicode-org.github.io/icu/userguide/icu_data/#how-data-loading-works
+    /// [2]: https://unicode-org.github.io/icu/userguide/icu_data/#icu-data-directory
+    pub fn open(path: Option<&Path>, a_type: Type, name: Option<&str>) -> Result<Self, common::Error> {
         let mut status = sys::UErrorCode::U_ZERO_ERROR;
-        let path_cstr = ffi::CString::new(path.to_str().unwrap())?;
-        let name_cstr = ffi::CString::new(name)?;
-        let type_cstr = ffi::CString::new(a_type)?;
+        let path_cstr = path.map(|s| { ffi::CString::new(s.to_str().expect("should never be a runtime error")).unwrap()});
+        let name_cstr = name.map(|s| { ffi::CString::new(s).expect("should never be a runtime error") } );
+        let type_cstr = ffi::CString::new(a_type.as_ref()).expect("should never be a runtime errror");
         let rep = unsafe {
             // Safety: we do what we must to call the underlying unsafe C API, and only return an
             // opaque enum, to ensure that no rust client code may touch the raw pointer.
@@ -121,17 +182,9 @@ impl crate::UDataMemory {
             // Would be nicer if there were examples of udata_open usage to
             // verify this.
             let rep: *const sys::UDataMemory = versioned_function!(udata_open)(
-                path_cstr.as_ptr(),
-                if type_cstr.is_empty() {
-                    std::ptr::null()
-                } else {
-                    type_cstr.as_ptr()
-                },
-                if name_cstr.is_empty() {
-                    std::ptr::null()
-                } else {
-                    name_cstr.as_ptr()
-                },
+                path_cstr.map(|s| s.as_ptr()).unwrap_or(std::ptr::null()),
+                type_cstr.as_ptr(),
+                name_cstr.map(|c| c.as_ptr()).unwrap_or(std::ptr::null()),
                 &mut status);
             // Sadly we can not use NonNull, as we can not make the resulting
             // type Sync or Send.
